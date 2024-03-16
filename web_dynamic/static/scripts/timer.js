00161e87dd4e1a0a4b8c9b7a13bccec461187630
Metrinenaliaka <metrinenam@gmail.com>
2024-03-16 12:37:26 +0300

    fixed bugs on timer

--- web_dynamic/static/scripts/timer.js
+++ web_dynamic/static/scripts/timer.js
@@ -1,28 +1,83 @@
 let timerInterval;
 let minutesRemaining = 25; // Initial value for countdown timer (25 minutes)
 let secondsRemaining = 0;
+let timerRunning = false;
 
 $(document).ready(function () {
   selectTimer(25); // Select "Pomodoro" on page load
+    $("#pomodoro").click(function () {
+      setTimeout(() => {
+        stopTimer(); // Stop the timer before switching
+        selectTimer(25);
+        $(this).addClass("selected");
+        $("#shortbreak, #longbreak").removeClass("selected");
+        $("body, html").css("background-color", "");
+      }, 50);
+    });
+
+    $("#shortbreak").click(function () {
+      setTimeout(() => {
+        stopTimer(); // Stop the timer before switching
+        selectTimer(5);
+        $(this).addClass("selected");
+        $("#pomodoro, #longbreak").removeClass("selected");
+        $("body, html").css("background-color", "#987C7D");
+      }, 50);
+    });
+
+    $("#longbreak").click(function () {
+      setTimeout(() => {
+        stopTimer(); // Stop the timer before switching
+        selectTimer(15);
+        $(this).addClass("selected");
+        $("#pomodoro, #shortbreak").removeClass("selected");
+        $("body, html").css("background-color", "#5A666E");
+      }, 50);
+    });
+    $("#start").click(function () {
+      if (!timerRunning) {
+        startTimer();
+        timerRunning = true;
+      }
+    });
+
+    $("#pause").click(function () {
+      if (timerRunning) {
+        pauseTimer();
+        timerRunning = false;
+      }
+    });
+
+    $("#stop").click(function () {
+      stopTimer();
+      timerRunning = false;
+    });
 });
 
 function startTimer() {
   // Start the timer
   timerInterval = setInterval(updateTimer, 1000);
+  timerRunning = true;
 }
 
 function pauseTimer() {
   // Pause the timer
   clearInterval(timerInterval);
+  timerRunning = false;
 }
 
 function stopTimer() {
   // Stop the timer
   clearInterval(timerInterval);
+  timerRunning = false;
   // Reset timer to initial value
-  minutesRemaining = 25;
-  secondsRemaining = 0;
-  updateDisplay();
+  if ($("#pomodoro").hasClass("selected")) {
+    selectTimer(25);
+  } else if ($("#shortbreak").hasClass("selected")) {
+    selectTimer(5);
+  } else if ($("#longbreak").hasClass("selected")) {
+    selectTimer(15);
+  }
 }
 
 function updateTimer() {
@@ -36,7 +91,7 @@ function updateTimer() {
     } else {
       // Timer has reached 0
       clearInterval(timerInterval);
-      alert("Time's up!");
+      timerRunning = false;
       return;
     }
   }
